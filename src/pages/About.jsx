7d9f485358a37cad3c92e604bc4ec2ebad94a1be
Fahmi Fahreza <fahmiiireza@gmail.com>
2024-01-05 14:47:24 +0100

    added image constant

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,25 @@
 const About = () => {
   return (
-    <div>Anjay</div>
-  )
-}
-export default About
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello I am{' '}
+        <span className='blue-gradient_text font-semibold drop-shadow-sm'>
+          Fahmi
+        </span>
+      </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Software Engineer based in Indonesia, specializing in technical
+          education by hands-on learning and building applications{' '}
+        </p>
+      </div>
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
+
+        <div className="mt-16 flex flex-wrap gap-12"></div>
+      </div>
+    </section>
+  );
+};
+export default About;
