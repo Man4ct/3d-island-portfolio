4532d2cd62a79efeb91e0872bf0a84953307a10d
Fahmi Fahreza <fahmiiireza@gmail.com>
2024-01-05 17:56:53 +0100

    added validation to not display button link if there's no link given

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -5,45 +5,48 @@ const InfoBox = ({ title, text, link, btnText }) => (
   <div className='info-box'>
     {title && <h1 className='info-title text-2xl font-bold mb-2 text-center'>{title}</h1>}
     <p className='font-medium sm:text-xl text-center'>{text}</p>
-    <Link to={link} className='neo-brutalism-white neo-btn'>
-      {btnText}
-      <img src={arrow} className='w-4 h-4 object-contain' alt='Arrow' />
-    </Link>
+    {link && (
+      <Link to={link} className='neo-brutalism-white neo-btn'>
+        {btnText}
+        <img src={arrow} className='w-4 h-4 object-contain' alt='Arrow' />
+      </Link>
+    )}
   </div>
 );
 
 const renderContent = {
-  1: (
-    <InfoBox
-      title='Welcome!'
-      text="Hi, I am Fahmi"
-    />
-  ),
-  2: (
-    <InfoBox
-      title='Web Development Journey'
-      text='Have been in the Industry of Web Development for 2 years and have picked up many skills along the way'
-      link='/about'
-      btnText='Learn more'
-    />
-  ),
-  3: (
-    <InfoBox
-      title='Impactful Projects'
-      text='Have done some impactful projects along the year, click to see more'
-      link='/projects'
-      btnText='Visit portfolio'
-    />
-  ),
-  4: (
-    <InfoBox
-      title='Let’s Discuss'
-      text='Looking for a developer to solve your problem? I’m just a few keystrokes away'
-      link='/contact'
-      btnText='Let’s talk'
-    />
-  ),
-};
+    1: (
+      <InfoBox
+        title='Welcome Aboard!'
+        text="Hey, I'm Fahmi, ready for a digital journey with you!"
+      />
+    ),
+    2: (
+      <InfoBox
+        title='My Web Odyssey'
+        text="With 2 years of web development experience, I've gathered a treasure trove of skills for our adventure!"
+        link='/about'
+        btnText='Discover more'
+      />
+    ),
+    3: (
+      <InfoBox
+        title='Projects that Echo'
+        text="Behold the echoes of impactful projects! Navigate to my portfolio and witness the resonance."
+        link='/projects'
+        btnText='Visit portfolio'
+      />
+    ),
+    4: (
+      <InfoBox
+        title='Let’s Unleash Creativity'
+        text="In search of a developer to unravel your challenges? Let's chat and weave the tapestry of solutions!"
+        link='/contact'
+        btnText='Chat with me'
+      />
+    ),
+  };
+  
 
 const HomeInfo = ({ currentStage }) => {
   return renderContent[currentStage] || null;
